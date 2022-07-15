dfdb2fc1b96dbf788c01079be52a956382c7531d
RAihan Rabby <57155292+RAIHANRABBY@users.noreply.github.com>
2022-07-15 22:39:13 +0600

    p

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,13 +18,13 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+# from django.conf.urls import 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
